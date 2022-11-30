721ca3fb6ec6f43adb15ec121d6b90928c240d55
Dishant Dilip Yashwante <113447476+dishant1996@users.noreply.github.com>
2022-11-30 20:30:03 +0530

    AddNewContacts

---
+++ Address_Book_Systems-UC2_AddNewContacts/AddressBook/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace AddressBook
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+
+            bool check = true;
+
+            while (check)
+            {
+                Console.WriteLine("Please select the Options :\n" +
+                    "1)Enter Details to add Contact in List\n" +
+                    "2)Add new Contact, Display and Stores contact in the list\n");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+
+                        AddressBook addobj = new AddressBook();
+                        Console.WriteLine("::::::::::::::::::::::::::");
+                        addobj.createContact();
+                        break;
+
+                    case 2:
+
+                        AddressBook addobj1 = new AddressBook();
+                        Console.WriteLine("::::::::::::::::::::::::::");
+                        addobj1.createContact();
+                        Console.WriteLine("::::::::::::::::::::::::::");
+                        Console.WriteLine("Entered Details of Person given as :");
+                        addobj1.Display();
+                        addobj1.addPerson();
+                        break;
+
+
+                    default:
+                        Console.WriteLine("Please select proper option");
+                        break;
+                }
+                check = false;
+                break;
+            }
+        }
+    }
+
+}
